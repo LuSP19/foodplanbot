6ac1582333406dd31cb64f17011a8a8b65d724ff
LuSP19 <semyon.starykh@gmail.com>
2022-04-06 11:28:29 +0500

    Remove comments from foodplan_bot.py

--- bot/management/commands/foodplan_bot.py
+++ bot/management/commands/foodplan_bot.py
@@ -407,13 +407,10 @@ def done(update, context):
 
 
 def main() -> None:
-    """Run the bot."""
-    # Create the Updater and pass it your bot's token.
     updater = Updater(settings.TG_BOT_TOKEN)
 
-    # Get the dispatcher to register handlers
     dispatcher = updater.dispatcher
-    # Add conversation handler with the states
+
     conv_handler = ConversationHandler(
         entry_points=[CommandHandler('start', start)],
         states={
@@ -511,12 +508,8 @@ def main() -> None:
 
     dispatcher.add_handler(conv_handler)
 
-    # Start the Bot
     updater.start_polling()
 
-    # Run the bot until you press Ctrl-C or the process receives SIGINT,
-    # SIGTERM or SIGABRT. This should be used most of the time, since
-    # start_polling() is non-blocking and will stop the bot gracefully.
     updater.idle()
 
 
